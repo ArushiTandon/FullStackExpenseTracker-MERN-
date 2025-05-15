4207ed221a002cd799973f0981a3506c877cafbe
Arushi Tandon <arushitandon007@gmail.com>
2025-05-15 16:54:38 +0530

    feat: Add Recent Income components with chart and transaction display to dashboard

--- frontend/expense_tracker_fs/src/pages/Dashboard/Home.jsx
+++ frontend/expense_tracker_fs/src/pages/Dashboard/Home.jsx
@@ -13,6 +13,8 @@ import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions';
 import Last30DaysExpenses from '../../components/Dashboard/Last30DaysExpenses';
+import RecentIncomeWithChart from '../../components/Charts/RecentIncomeWithChart';
+import RecentIncome from '../../components/Dashboard/RecentIncome';
 
 const Home = () => {
   useUserAuth();
@@ -49,7 +51,7 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className='my-5 mx-auto'>
-        {/* <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
           <InfoCard
           icon={<IoMdCard />}
           label="Total Balance"
@@ -72,7 +74,7 @@ const Home = () => {
           />
 
 
-        </div> */}
+        </div>
 
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
           <RecentTransactions
@@ -93,6 +95,16 @@ const Home = () => {
 
           <Last30DaysExpenses
           data={dashboardData?.last30DaysExpenses?.transactions || []}
+          />
+
+          <RecentIncomeWithChart
+            data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
+            totalIncome={dashboardData?.totalIncome || 0}
+          />
+
+          <RecentIncome
+            transactions={dashboardData?.last60DaysIncome?.transactions || []}
+            onSeeMore={() => navigate("/income")}
           />
         </div>
       </div>
